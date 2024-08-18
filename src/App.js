2357393310848b716a6998a010a0505a98b9d896
Xcho <khachikmatevosyan41@gmail.com>
2024-08-19 03:05:09 +0400

    Add store and binding to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './store/store';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        
+      </div>
+    </Provider>
   );
 }
 
