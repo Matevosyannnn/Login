59caaa19451b51b21c2bd30864e9799667ca0064
Xcho <khachikmatevosyan41@gmail.com>
2024-08-19 02:57:49 +0400

    Add folder 'assets' and add images

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const IMAGES = {
+    greenLogo: require("./logoGreen.png"),
+    whiteLogo: require("./whiteLogo.png"),
+    backgroundImg: require("./1670934465_cerenas-club-p-zelen-fon-vkontakte-5.jpg"),
+}
+
+export default IMAGES
