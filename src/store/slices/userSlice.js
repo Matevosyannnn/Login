64204fe746a89d91bc48b43699aaf2fca629c1e8
Xcho <khachikmatevosyan41@gmail.com>
2024-08-19 15:10:43 +0400

    Createing 'getUser' reducer

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,5 +1,5 @@
 import { asyncThunkCreator, buildCreateSlice } from "@reduxjs/toolkit";
-import { loginAPI } from "../API/userAPI";
+import { getUserAPI, loginAPI } from "../API/userAPI";
 
 const createAuthSlice = buildCreateSlice({
     creators: { asyncThunk: asyncThunkCreator }
@@ -14,18 +14,34 @@ const userSlice = createAuthSlice({
                 const { dispatch, rejectWithValue } = thunkAPI
                 try {
                     const response = await loginAPI(data)
+                    dispatch(userActions.getUser(response.data.token))
                     return response
                 } catch (error) {
 
                 }
             },
             {
-            fulfilled: (state, {payload}) => {
-                localStorage.setItem('token', payload.data.token)
-                localStorage.setItem('refreshToken', payload.data.refreshToken)
-                
+                fulfilled: (state, {payload}) => {
+                    localStorage.setItem('token', payload.data.token)
+                    localStorage.setItem('refreshToken', payload.data.refreshToken)
+                },
             },
+        ),
+        getUser: create.asyncThunk(
+            async (data, thunkAPI) => {
+                const { dispatch, rejectWithValue } = thunkAPI
+                try {
+                    const response = await getUserAPI(data)
+                    return response
+                } catch (error) {
+                    
+                }
             },
+            {
+                fulfilled: (state, {payload}) => {
+                    return state = payload.data
+                }
+            }
         )
     })
 })
