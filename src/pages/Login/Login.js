73896ded8a5bf5f9e854f3d996c1d04c63dc4492
Xcho <khachikmatevosyan41@gmail.com>
2024-08-19 16:25:11 +0400

    Adding a request to the server to get the user in the login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import LoginForm from '../../components/LoginForm/LoginForm'
 import IMAGES from '../../assets/images'
 import useDevice from '../../hooks/useDevice'
 import style from './Login.module.css'
 import Header from '../../components/Header/Header'
-
+import { useDispatch } from 'react-redux'
+import { userActions } from '../../store/slices/userSlice'
 
 const Login = () => {
   const { isMobile } = useDevice()  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      dispatch(userActions.getUser(token))
+    }
+  }, [])
 
   return (
     <div className={style.wrapper}>
