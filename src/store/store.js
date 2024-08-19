c5e760cd5f5bf0033888b9e76fd9d122898acf62
Xcho <khachikmatevosyan41@gmail.com>
2024-08-19 20:06:43 +0400

    Add default middlewares to store

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,19 @@ import { userReducer } from "./slices/userSlice";
 const store = configureStore({
     reducer: {
         user: userReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => [
+        ...getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    'user/login/rejected', 
+                    'user/getUser/rejected',
+                    'user/refreshToken/rejected',
+                ],
+            },
+        }),
+
+    ]
 })
 
 export default store
